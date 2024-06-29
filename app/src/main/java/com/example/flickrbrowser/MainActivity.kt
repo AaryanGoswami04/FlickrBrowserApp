b73264f90df9622406fe786fe2be9ebef2fc6b75
AaryanGoswami04 <aaryan2004.aa@gmail.com>
2024-06-29 11:01:45 +0530

    Uri Builder

--- app/src/main/java/com/example/flickrbrowser/MainActivity.kt
+++ app/src/main/java/com/example/flickrbrowser/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.flickrbrowser
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import com.google.android.material.snackbar.Snackbar
@@ -11,6 +12,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.example.flickrbrowser.databinding.ActivityMainBinding
+import androidx.appcompat.widget.Toolbar;
 
 class MainActivity : AppCompatActivity(), GetRawData.OnDownloadComplete, GetFlickrData.OnDataAvailable {
     private val TAG = "MainActivity"
@@ -30,13 +32,25 @@ class MainActivity : AppCompatActivity(), GetRawData.OnDownloadComplete, GetFlic
 //        val navController = findNavController(R.id.nav_host_fragment_content_main)
 //        appBarConfiguration = AppBarConfiguration(navController.graph)
 //        setupActionBarWithNavController(navController, appBarConfiguration)
-
+        val url = createUri("https://api.flickr.com/services/feeds/photos_public.gne","android,oreo", "en-us", true)
         val getRawData = GetRawData(this)
-        getRawData.execute("https://api.flickr.com/services/feeds/photos_public.gne?tags=android,oreo&format=json&nojsoncallback=1")
+        getRawData.execute(url)
 
         Log.d(TAG, "OnCreate ends")
     }
+    private fun createUri(baseURL: String, searchCriteria: String, lang: String, matchAll: Boolean): String {
+        Log.d(TAG, "createUrl starts")
 
+        var uri = Uri.parse(baseURL)
+        var builder = uri.buildUpon()
+        builder = builder.appendQueryParameter("tags", searchCriteria)
+        builder = builder.appendQueryParameter("tagmode", if (matchAll) "ALL" else "ANY")
+        builder = builder.appendQueryParameter("format", "json")
+        builder = builder.appendQueryParameter("nojsoncallback", "1")
+        uri = builder.build()
+
+        return uri.toString()
+    }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         Log.d(TAG, "onCreateOptionsMenu called")
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -54,11 +68,11 @@ class MainActivity : AppCompatActivity(), GetRawData.OnDownloadComplete, GetFlic
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = findNavController(R.id.nav_host_fragment_content_main)
+//        return navController.navigateUp(appBarConfiguration)
+//                || super.onSupportNavigateUp()
+//    }
 
     //    companion object {
 //        private const val TAG = "MainActivity"
