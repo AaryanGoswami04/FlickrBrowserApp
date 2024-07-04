23db1b63a4c50470c4109c10569198e567132faf
AaryanGoswami04 <aaryan2004.aa@gmail.com>
2024-07-04 22:45:26 +0530

    Search Feature

--- app/src/main/java/com/example/flickrbrowser/MainActivity.kt
+++ app/src/main/java/com/example/flickrbrowser/MainActivity.kt
@@ -78,7 +78,10 @@ class MainActivity : BaseActivity(), GetRawData.OnDownloadComplete, GetFlickrDat
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_search -> {
+                startActivity(Intent(this, SearchActivity::class.java))
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
--- app/src/main/java/com/example/flickrbrowser/SearchActivity.kt
+++ app/src/main/java/com/example/flickrbrowser/SearchActivity.kt
@@ -2,6 +2,8 @@ package com.example.flickrbrowser
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.widget.SearchView
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -12,6 +14,7 @@ import com.example.flickrbrowser.databinding.ActivitySeachBinding
 
 class SearchActivity : BaseActivity() {
     private val TAG = "SearchActivity"
+    private var searchView: SearchView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, ".onCreate starts")
@@ -20,4 +23,9 @@ class SearchActivity : BaseActivity() {
         activateToolbar(true)
         Log.d(TAG, ".onCreate ENDS")
    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_search, menu)
+        return true
+    }
 }
