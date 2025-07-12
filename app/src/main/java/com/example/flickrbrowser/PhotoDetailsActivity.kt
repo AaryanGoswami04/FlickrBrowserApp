355758a304088f4d9303f46acae0132d430cfb9a
AaryanGoswami04 <aaryan2004.aa@gmail.com>
2025-07-13 00:03:37 +0530

    Bug fix

--- app/src/main/java/com/example/flickrbrowser/PhotoDetailsActivity.kt
+++ app/src/main/java/com/example/flickrbrowser/PhotoDetailsActivity.kt
@@ -1,12 +1,14 @@
 package com.example.flickrbrowser
+
 import android.os.Bundle
 import android.util.Log
+import com.bumptech.glide.Glide
 import com.example.flickrbrowser.databinding.ActivityPhotoDetailsBinding
-import com.squareup.picasso.Picasso
 
 class PhotoDetailsActivity : BaseActivity() {
 
     private lateinit var binding: ActivityPhotoDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPhotoDetailsBinding.inflate(layoutInflater)
@@ -17,11 +19,19 @@ class PhotoDetailsActivity : BaseActivity() {
         val photo: Photo? = intent.getParcelableExtra(PHOTO_TRANSFER)
 
         photo?.let {
+            binding.contentPhotoDetails.photoTitle.text =
+                resources.getString(R.string.photo_title_text, it.title)
 
-            binding.contentPhotoDetails.photoTitle.text = resources.getString(R.string.photo_title_text, photo.title)
-            binding.contentPhotoDetails.photoAuthor.text = photo.author
-            binding.contentPhotoDetails.photoTags.text = resources.getString(R.string.photo_tags_text, photo.tags)
-            Picasso.get().load(it.image).placeholder(R.drawable.placeholder).into(binding.contentPhotoDetails.photoImage)
+            binding.contentPhotoDetails.photoAuthor.text = it.author
+
+            binding.contentPhotoDetails.photoTags.text =
+                resources.getString(R.string.photo_tags_text, it.tags)
+
+            Glide.with(this)
+                .load(it.image)
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.placeholder)
+                .into(binding.contentPhotoDetails.photoImage)
         } ?: run {
             Log.e("PhotoDetailsActivity", "Photo data is missing!")
         }
